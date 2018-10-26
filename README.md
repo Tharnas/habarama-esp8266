30aa1bd3715d66c3f85edc22c3b97c4dc0576ab6
mathias <mathias.jutz@jutz.eu>
2018-10-26 14:04:49 +0200

    Update documentation. Refector code for better readability.

--- README.md
+++ README.md
@@ -16,29 +16,34 @@ IDE: [Arduino](https://www.arduino.cc/en/Main/Software)
 ### Dependencies
 * ESP8266 Board ([Github](https://github.com/esp8266/Arduino))
 * PubSubClient 2.6.0 ([Github](https://github.com/knolleary/pubsubclient))
-* ArduinoJson 5.13.2 ([Github](https://github.com/bblanchon/ArduinoJson))
+* ArduinoJson 5.13.3 ([Github](https://github.com/bblanchon/ArduinoJson))
+* WifiManager 0.14.0 ([Github](https://github.com/tzapu/WiFiManager))
 
 ## Installation
 
 1. Install Arduino IDE from link above
 1. Add ESP8266 Addon (See tutorial from [Sparkfun](https://learn.sparkfun.com/tutorials/esp8266-thing-hookup-guide/installing-the-esp8266-arduino-addon))
-1. Install the PubSubClient and ArduinoJson library
+1. Install all dependencies with library manager
    * *Sketch > Include Library > Manage Libraries*
-   * Search for PubSubClient and ArduinoJson and install the appropriate versions.
+   * Search for each dependency (see list above) exept ESP8266 Board and install the appropriate versions.
 1. Download this repository and open it with the IDE
-1. Enter the SSID and Password of the Wifi where ESP8266 will connect to in the following two defines:
-    ```` 
-    #define wifi_ssid "......."
-    #define wifi_password "........"
-    ````
-1. Also enter the `name`, `type` and `location` of the sensor:
+1. Enter the `name`, `type` and `location` of the sensor:
     ````
     #define sensor_name "......"
     #define sensor_type "......"
     #define sensor_location "......"
     ````
+1. Configure actors: (pin, 0, top of actor)
+    ````
+    actor_type actors[] = {
+      // {D0, 0, "actor.<location>.<actor name 1>"},
+      // {D1, 0, "actor.<location>.<actor name 2>"}
+    };
+    ````
 1. Connect ESP8266 to the computer
 1. Select the board from *Tools > Board > NodeMCU 1.0 (ESP-12E Module)*
 1. Select the COM port where ESP8266 is connected *Tools > Board > Port > COMxx*
 1. Push `RST` and `FLASH` button, release `RST` and then release `FLASH`
-1. Download the firmware from the Arduino IDE
+1. Flash the firmware from the Arduino IDE
+1. The microcontroller will create a new access point. Connect there and navigate with a browser to `192.168.4.1`.
+1. Provide in the webinterface the SSID and password for your network.
--- habarama/habarama.ino
+++ habarama/habarama.ino
@@ -1,19 +1,19 @@
-#include <ESP8266WiFi.h>
+#include <ESP8266WiFi.h>          //ESP8266 Core WiFi Library
 
 #include <DNSServer.h>            //Local DNS Server used for redirecting all requests to the configuration portal
 #include <ESP8266WebServer.h>     //Local WebServer used to serve the configuration portal
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager WiFi Configuration Magic
 
-
-#include <PubSubClient.h>
-#include <ArduinoJson.h>
+#include <PubSubClient.h>         // Client to connect to cloud via mqtt
+#include <ArduinoJson.h>          // Library to parse Json. Used for payload of mqtt
 #include <stdio.h>
 
-
+// configuration for sensor
 #define sensor_name "......"
 #define sensor_type "......"
 #define sensor_location "......"
 
+// configuration for mqtt server
 #define mqtt_server "broker-amq-mqtt-ssl-57-hogarama.cloud.itandtel.at"
 #define mqtt_server_port 443
 #define mqtt_user "mq_habarama"
@@ -23,27 +23,46 @@ WiFiClientSecure espClient;
 PubSubClient client(espClient);
 
 typedef struct {
-  byte pin;
-  long ticks_until;
-  String topic;
+  byte pin; // the pin where the actor is connected to
+  long ticks_until; // the ticks until the pin should be triggered. Should be initialized with 0.
+  String topic; // the topic this actor should listen to
 } actor_type;
 
+// configuration for actor.
 actor_type actors[] = {
   // {D0, 0, "actor.<location>.<actor name 1>"},
   // {D1, 0, "actor.<location>.<actor name 2>"}
 };
 
+long lastSensorDataSent = 0;
+long lastReconnect = 0;
+
 void setup() {
   Serial.begin(115200);
 
-  WiFiManager wifiManager;
-  wifiManager.autoConnect();
+  initializeWifi();
 
+  initializeActors();
+
+  initializeMqtt();
+}
+
+
+void initializeWifi() {
+  WiFiManager wifiManager;
+  // wifiManager.resetSettings(); // This can be used to rest wifi settings.
+  wifiManager.autoConnect();
+}
+
+void initializeActors() {
   for (byte i = 0; i < (sizeof(actors) / sizeof(*actors)); i++) {
+    actors[i].ticks_until = 0;
     pinMode(actors[i].pin, OUTPUT);
     digitalWrite(actors[i].pin, LOW);
   }
+}
 
+void initializeMqtt() {
   espClient.allowSelfSignedCerts();
   client.setServer(mqtt_server, mqtt_server_port);
   client.setCallback(mqtt_callback);
@@ -53,16 +72,17 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("mqtt topic: ");
   Serial.println(topic);
 
-  
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& root = jsonBuffer.parseObject(payload);
 
   for (byte i = 0; i < (sizeof(actors) / sizeof(*actors)); i++) {
+
+    // Check if topic matches to actor
     if (strcmp(topic, actors[i].topic.c_str()) == 0) {
       Serial.println("actor found");
 
       if (!root.success()) {
-        Serial.println("ParsenObject() failed");
+        Serial.println("Could not parse payload from mqtt");
         return;
       }
 
@@ -75,87 +95,111 @@ void mqtt_callback(char* topic, byte* payload, unsigned int length) {
         Serial.println("Unknown actor type");
         return;
       }
-
-      break;
     }
   }
 }
 
 void setGpio(actor_type *actor, long duration) {
+  Serial.print("Setting actor to hight: ");
+  Serial.println(actor->topic);
+
   actor->ticks_until = millis() + duration;
 
   digitalWrite(actor->pin, HIGH);
-  Serial.println("Actor set");
 }
 
 void reconnect() {
-  // Loop until we're reconnected
-  while (!client.connected()) {
+  // reconnect when not connected and 5s passed since last try.
+  if (!client.connected() && (lastReconnect == 0 || (millis() - lastReconnect) > 5000)) {
     Serial.print("Attempting MQTT connection...");
 
     // Attempt to connect
     if (client.connect("ESP8266Client", mqtt_user, mqtt_password)) {
       Serial.println("connected");
 
-      for (byte i = 0; i < (sizeof(actors) / sizeof(*actors)); i++) {
-        if (actors[i].topic.length()) {
-          if (client.subscribe(actors[i].topic.c_str())) {
-            Serial.println("Successfully subscribed");
-          }
-          else {
-            Serial.println("Failed to subscribe....");
-          }
-        }
-      }
+      subscribeToMqtt();
     } else {
       Serial.print("failed, rc=");
-      Serial.print(client.state());
-      Serial.println(" try again in 5 seconds");
-      // Wait 5 seconds before retrying
-      delay(5000);
+      Serial.println(client.state());
     }
   }
 }
 
-long lastMsg = 0;
+void subscribeToMqtt() {
+
+  // subscribe all actors to mqtt
+  for (byte i = 0; i < (sizeof(actors) / sizeof(*actors)); i++) {
+    if (actors[i].topic.length()) {
+      if (client.subscribe(actors[i].topic.c_str())) {
+        Serial.println("Successfully subscribed");
+      }
+      else {
+        Serial.println("Failed to subscribe....");
+      }
+    }
+  }
+
+}
 
 void loop() {
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.loop();
 
+  // set pin of actor to low when ticks_until passed.
   for (byte i = 0; i < (sizeof(actors) / sizeof(*actors)); i++) {
-    if (actors[i].ticks_until < millis()) {
+    if (actors[i].ticks_until < millis() && digitalRead(actors[i].pin)) {
+      Serial.print("Setting actor to low: ");
+      Serial.println(actors[i].topic);
       digitalWrite(actors[i].pin, LOW);
     }
   }
 
-  long now = millis();
-  if (now - lastMsg > 60000) {
-    lastMsg = now;
-
-    int val = analogRead(0);
-
-    String payload;
-    StaticJsonBuffer<200> jsonBuffer;
-
-    JsonObject& root = jsonBuffer.createObject();
-    //  '{{"sensorName": "name", "type": "type", "value": value, "location": "location", "version": 1 }}'
-    root["sensorName"] = sensor_name;
-    root["type"] = sensor_type;
-    root["value"] = val;
-    root["location"] = sensor_location;
-    root["version"] = 1;
-
-    root.printTo(payload);
-
-    Serial.print("Sending: ");
-    Serial.println(payload);
-    char payloadChar[payload.length() + 1];
-
-    payload.toCharArray(payloadChar, payload.length() + 1);
-
-    client.publish("habarama", payloadChar, true);
+  if (!client.connected()) {
+    reconnect();
   }
+
+  if (client.connected()) {
+    client.loop();
+
+    // send each 60s sensor data to cloud.
+    long now = millis();
+    if (now - lastSensorDataSent > 60000) {
+      lastSensorDataSent = now;
+
+      sendSensorData();
+    }
+  }
+}
+
+void sendSensorData() {
+  String payload;
+
+  // read sensor data
+  int val = analogRead(0);
+
+  /* Create json payload.
+     Example:
+     {
+       "sensorName": "<name of sensor>",
+       "type": "<type of sensor>",
+       "value": <value read from sensor (0 - 1023)>,
+       "location": "<location of sensor>",
+       "version": <version of this sensor>
+      }
+  */
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& root = jsonBuffer.createObject();
+  root["sensorName"] = sensor_name;
+  root["type"] = sensor_type;
+  root["value"] = val;
+  root["location"] = sensor_location;
+  root["version"] = 1;
+
+  root.printTo(payload);
+
+  Serial.print("Sending: ");
+  Serial.println(payload);
+  char payloadChar[payload.length() + 1];
+
+  payload.toCharArray(payloadChar, payload.length() + 1);
+
+  client.publish("habarama", payloadChar, true);
 }
